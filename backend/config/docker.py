375609634992f8c59e659ef89669a024d1c48a35
munanc <1208418170@qq.com>
2024-03-07 23:39:08 +0800

    fix docker config

---
+++ backend/config/docker.py
@@ -0,0 +1,4 @@
+# 配置文件
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@mysql:3306/py_demo'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend/start.py
+++ backend/start.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from db.database import get_db, init_db
+from controller.index import register_routes
+from flask_cors import CORS
 
-app = Flask(__name__) #记住这里的变量名app
+app=Flask(__name__)
+app.config.from_object('config.docker.Config')  # 导入配置信息
+db = get_db()
+db.init_app(app)
 
-@app.route('/')
-def hello():
-    return 'hello docker&flask'
+with app.app_context():
+    init_db()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+register_routes(app)
+
+CORS(app, supports_credentials=True)
+
+if __name__=='__main__':
+    # app.run(host='0.0.0.0',debug=True,port='8000')
+    app.run(debug=True) # 启动 Flask 应用
